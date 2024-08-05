0b2b6104d4bfe7480346099c1e5109276f556e7a
fselmakpnr <121101684+fselmakpnr@users.noreply.github.com>
2024-08-05 22:31:11 +0300

    Add files via upload

---
+++ BHarfi/src/BHarfi.java
@@ -0,0 +1,35 @@
+
+public class BHarfi {
+
+	public static void main(String[] args) {
+		
+		String[][] harf=new String[5][5];
+		
+		for(int i=0;i<harf.length;i++)
+		{
+			for(int j=0; j<harf[i].length;j++)
+			{
+				if(i==0 ||i==2 || i==4)
+				{
+					harf[i][j]="*";
+				}
+				else if(j==0 || j==4)
+				{
+					harf[i][j]="*";
+				}
+				else
+					harf[i][j]=" ";
+			}
+		}
+		
+		for(String[]row:harf)
+		{
+			for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+		}
+
+	}
+
+}
