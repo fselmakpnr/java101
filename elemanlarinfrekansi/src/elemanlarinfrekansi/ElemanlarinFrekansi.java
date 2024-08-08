8abf4fd4380efcc9c251aa4b4cac341034166500
fselmakpnr <121101684+fselmakpnr@users.noreply.github.com>
2024-08-08 16:50:48 +0300

    Add files via upload

---
+++ elemanlarinfrekansi/src/elemanlarinfrekansi/ElemanlarinFrekansi.java
@@ -0,0 +1,46 @@
+package elemanlarinfrekansi;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ElemanlarinFrekansi {
+
+	public static void main(String[] args) {
+		
+		int dizi[]= {10, 20, 20, 10, 10, 20, 5, 20,8};
+		
+		System.out.print("Dizi:");
+		
+		for(int i=0;i<dizi.length;i++)
+		{
+			System.out.print(" "+dizi[i]);
+			
+		}
+		 Arrays.sort(dizi);
+		 
+		 int simdikiSayi=dizi[0];
+		 int adet=1;
+		 
+		 System.out.println();
+		 
+		 for(int i=1;i<dizi.length;i++)
+			{
+				if(simdikiSayi==dizi[i])
+				{
+					adet++;
+					
+				}
+				else 
+				{
+					
+	                System.out.println(simdikiSayi + " sayısı " + adet + " kez tekrar edilmiştir.");
+	               
+	                simdikiSayi = dizi[i];
+	                
+	                adet = 1;
+	            }
+			}
+		 
+		 System.out.println(simdikiSayi + " sayısı " + adet + " kez tekrar edilmiştir.");
+	}
+
+}
