77389c76a5e2051cc9ae5653cbc4c3d9225613e3
fselmakpnr <121101684+fselmakpnr@users.noreply.github.com>
2024-08-11 18:25:15 +0300

    atm projesi

---
+++ atmprojesi/src/atmprojesi/AtmProjesi.java
@@ -0,0 +1,89 @@
+package atmprojesi;
+
+import java.util.Scanner;
+
+public class AtmProjesi {
+
+    public static void main(String[] args) {
+
+        String userName, password;
+        Scanner input = new Scanner(System.in);
+        int right = 3;
+        int balance = 1500;
+        int select;
+        
+        while (right > 0) {
+            System.out.print("Kullanıcı Adınız: ");
+            userName = input.nextLine();
+
+            System.out.print("Parolanız: ");
+            password = input.nextLine();
+
+            if (userName.equals("patika") && password.equals("dev123")) {
+                System.out.println("Merhaba, Kodluyoruz Bankasına Hoşgeldiniz!");
+                
+                do {
+                    System.out.println("1-Para yatırma\n" +
+                            "2-Para Çekme\n" +
+                            "3-Bakiye Sorgula\n" +
+                            "4-Çıkış Yap");
+                    System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz: ");
+                    select = input.nextInt();
+
+                    switch (select) 
+                    {
+                        case 1:
+                            System.out.print("Yatırmak istediğiniz para miktarı: ");
+                            int depositAmount = input.nextInt();
+                            balance += depositAmount;
+                            System.out.println("Yeni bakiyeniz: " + balance);
+                            break;
+
+                        case 2:
+                            System.out.print("Çekmek istediğiniz para miktarı: ");
+                            int withdrawAmount = input.nextInt();
+                            if (withdrawAmount > balance) 
+                            {
+                                System.out.println("Bakiye yetersiz.");
+                            }
+                            else {
+                                balance -= withdrawAmount;
+                                System.out.println("Yeni bakiyeniz: " + balance);
+                            }
+                            break;
+
+                        case 3:
+                            System.out.println("Bakiyeniz: " + balance);
+                            break;
+
+                        case 4:
+                            System.out.println("Çıkış yapılıyor...");
+                            break;
+
+                        default:
+                            System.out.println("Geçersiz bir işlem seçtiniz. Lütfen tekrar deneyin.");
+                            break;
+                    }
+                } 
+                while (select != 4);
+                
+                System.out.println("Tekrar görüşmek üzere.");
+                break;
+                
+            } 
+            else {
+                right--;
+                System.out.println("Hatalı kullanıcı adı veya şifre. Tekrar deneyiniz.");
+                if (right == 0)
+                {
+                    System.out.println("Hesabınız bloke olmuştur lütfen banka ile iletişime geçiniz.");
+                } 
+                else 
+                {
+                    System.out.println("Kalan Hakkınız: " + right);
+                }
+            }
+        }
+        input.close(); 
+    }
+}
